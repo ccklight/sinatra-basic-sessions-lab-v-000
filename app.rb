d66a50096b9fc9285a2c3b7140667ade512f2fa8
Cheryl K <cherylkelson@icloud.com>
2019-03-21 13:39:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,3 +28,24 @@ end
   end
 
 end
+*******
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, 'flatiron'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+end
+*******
